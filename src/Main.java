67dfc925b9b2fbb720365c51243edf263d491aed
Shuvaeva Anastasia <sunny.shuvaeva@mail.ru>
2021-10-05 21:14:09 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 5000;
+        int persent = 20;
+        int bonus = amount / persent;
+        System.out.println("Ваш бонус:" + bonus);
+    }
+}
